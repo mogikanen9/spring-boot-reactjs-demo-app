113895fa648f7f504ada2cfb822bd5caca4dd4dc
Vlad <mogikanensoftware@gmail.com>
2017-08-28 20:44:49 -0400

    init boot setup with gradle

---
+++ src/main/java/com/mogikanensoftware/spring/demo/app/SpringBootReactjsDemoAppApplication.java
@@ -0,0 +1,12 @@
+package com.mogikanensoftware.spring.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactjsDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootReactjsDemoAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/mogikanensoftware/spring/demo/app/SpringBootReactjsDemoAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mogikanensoftware.spring.demo.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootReactjsDemoAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

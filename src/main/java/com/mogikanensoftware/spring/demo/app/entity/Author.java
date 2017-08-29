f228a95443f32aafaaed383fe9f5e13df6fe6b7c
Vlad <mogikanensoftware@gmail.com>
2017-08-28 21:11:47 -0400

    Author Enity and Rest service
     * Author entity defined
     * AuthorRepository defined
     * BookController with listAuthors service method

---
+++ src/main/java/com/mogikanensoftware/spring/demo/app/entity/Author.java
@@ -0,0 +1,57 @@
+package com.mogikanensoftware.spring.demo.app.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="AUTHOR")
+public class Author {
+
+	private @Id @GeneratedValue Long id;
+	
+	@Column(name="FIRST_NAME")
+	private String firstName;
+	
+	@Column(name="LAST_NAME")
+	private String lastName;
+
+	
+	public Author() {
+		super();
+	}
+
+	public Author(Long id, String firstName, String lastName) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into author(first_name, last_name) values ('Yakov', 'Fain')

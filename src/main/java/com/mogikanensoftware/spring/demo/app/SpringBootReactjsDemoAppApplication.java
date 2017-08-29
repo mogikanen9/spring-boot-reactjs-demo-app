453965e90e7254094c61b2b4a07c6dc813394d7a
Vlad <mogikanensoftware@gmail.com>
2017-08-29 11:47:42 -0400

    misc updates
     - RepositoryRestMvcAutoConfiguration turned off
     - more data init records for author table
     - app properties updates

--- src/main/java/com/mogikanensoftware/spring/demo/app/SpringBootReactjsDemoAppApplication.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/SpringBootReactjsDemoAppApplication.java
@@ -1,9 +1,12 @@
 package com.mogikanensoftware.spring.demo.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude=RepositoryRestMvcAutoConfiguration.class)
 public class SpringBootReactjsDemoAppApplication {
 
 	public static void main(String[] args) {

36271ea36e25c68c9a2cb4195caba154f4166116
Vlad <mogikanensoftware@gmail.com>
2017-08-31 23:26:17 -0400

    BookList and PublisherList templates

---
+++ client/src/components/ui/publisher/PublisherList.js
@@ -0,0 +1,35 @@
+import { Component } from 'react'
+import fetch from 'isomorphic-fetch'
+import {CustomPageHeader} from '../CustomPageHeader'
+
+export class PublisherList extends Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            publishers: [],
+            loading: false
+        }
+    }
+
+    getPublishersFromApiAsync(){
+    }
+
+    componentDidMount(){
+       this.getPublishersFromApiAsync()
+    }
+
+    render() {
+
+        return (
+            <div className="container">
+                 <CustomPageHeader headerTitle="Publishers"/>
+                 <div className="row">
+                     <div className="col-md-12">
+                           
+                      </div>      
+                </div>
+             </div>    
+        )    
+   }     
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -3,6 +3,8 @@ import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { Home } from './components/ui/home/Home'
 import  { Whoops404 } from './components/ui/err/Whoops404'
 import { AuthorList } from './components/ui/author/AuthorList'
+import { BookList } from './components/ui/book/BookList'
+import { PublisherList } from './components/ui/publisher/PublisherList'
 
 const routes = (
     <Router>
@@ -10,6 +12,8 @@ const routes = (
             <Route exact path="/" component={Home} />
             <Route exact path="/home" component={Home} />
             <Route path="/authors" component={AuthorList} />
+            <Route path="/books" component={BookList} />
+            <Route path="/publishers" component={PublisherList} />
             <Route path="*" component={Whoops404} />
         </Switch>    
     </Router>

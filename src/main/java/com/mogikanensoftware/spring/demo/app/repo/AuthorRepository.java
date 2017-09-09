8195118f47c8f861573b2319fc8bf7d32855797a
Vlad <mogikanensoftware@gmail.com>
2017-09-08 22:00:31 -0400

    pagination support enabled on backend

--- src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
@@ -1,6 +1,6 @@
 package com.mogikanensoftware.spring.demo.app.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +9,6 @@ import com.mogikanensoftware.spring.demo.app.entity.Author;
 
 @CrossOrigin(origins = "http://localhost:3000", methods = { RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE},maxAge = 3600)
 @RepositoryRestResource(exported=true)
-public interface AuthorRepository extends CrudRepository<Author, Long>{
+public interface AuthorRepository extends PagingAndSortingRepository<Author, Long>{
 
 }

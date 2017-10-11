ac76df4269ec8c00baf7648be42cd54ad7a45513
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-10-11 00:41:23 -0400

    basic workable security setup (protected welcome page and 2 users) based on thymeleaf

--- src/main/java/com/mogikanensoftware/spring/demo/app/config/SecurityConfiguration.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/config/SecurityConfiguration.java
@@ -7,22 +7,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import com.mogikanensoftware.spring.demo.app.service.CustomUserDetailService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-	@Autowired
-	protected CustomUserDetailService userDetailsService;
-
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.userDetailsService);
+		auth.inMemoryAuthentication().withUser("user").password("password").roles("VIEW")
+		.and().withUser("admin")
+				.password("password").roles("VIEW","EDIT");
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/built/**", "/main.css", "/api/v1/**").permitAll().and().csrf().disable();
+		http.authorizeRequests().antMatchers("/welcome**").hasAnyRole("VIEW").anyRequest().authenticated().and()
+				.formLogin().loginPage("/login").permitAll().and().logout().permitAll().and().csrf().disable();
 	}
 }

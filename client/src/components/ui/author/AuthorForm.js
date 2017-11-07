7176baa52198c3d833dcd3542a5ed63603235aaf
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-11-06 21:44:52 -0500

    migrated to create-react-app: workable version

--- client/src/components/ui/author/AuthorForm.js
+++ client/src/components/ui/author/AuthorForm.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Component} from 'react'
 import {FieldGroup} from 'react-bootstrap'
 import {Button} from 'react-bootstrap'
--- client/src/components/ui/home/Home.js
+++ client/src/components/ui/home/Home.js
@@ -1,4 +1,4 @@
-import {Component} from 'react'
+import React, {Component} from 'react'
 import {CustomPageHeader} from '../CustomPageHeader'
 
 export class Home extends Component{

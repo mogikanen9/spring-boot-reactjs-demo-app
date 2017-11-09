3aa7c75dd7da83bc305407838a029e789dd0b421
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-11-08 22:15:27 -0500

    added test method for routes.js->authorList

--- client/src/routes.test.js
+++ client/src/routes.test.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import routes from './routes';
+import {routes, authorList} from './routes';
 
-it('renders without crashing', () => {
+it('routes component renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(routes, div);
 });
+
+it('authorList without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(authorList, div);
+});
+

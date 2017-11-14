b23f7c2aec5eeb1a9e59318aa011226c3a4dab77
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-11-13 21:44:26 -0500

    BookRepository initial implementation

--- src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
@@ -1,7 +1,6 @@
 package com.mogikanensoftware.spring.demo.app.repo;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;

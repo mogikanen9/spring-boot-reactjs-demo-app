013b0ffcd4d3cfcfd7f94183b32003a083e65ef5
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-12-20 10:48:36 -0500

    CORS is enabled only for LOCAL-DEV profile

--- src/main/java/com/mogikanensoftware/spring/demo/app/config/SpringDataRestCustomization.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/config/SpringDataRestCustomization.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
 
 @Configuration
+@Profile("LOCAL-DEV")
 public class SpringDataRestCustomization extends RepositoryRestConfigurerAdapter {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(SpringDataRestCustomization.class);
--- src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
@@ -3,8 +3,6 @@ package com.mogikanensoftware.spring.demo.app.repo;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.mogikanensoftware.spring.demo.app.entity.Author;
 

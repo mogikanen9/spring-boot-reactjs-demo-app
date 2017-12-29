0a8b05ffafcd7c60ad3f0e2a798118def3c9a953
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-12-28 23:25:02 -0500

    AuthorInfo projection was created for AuthorRepository, not enbaled though

---
+++ src/main/java/com/mogikanensoftware/spring/demo/app/projections/AuthorInfo.java
@@ -0,0 +1,21 @@
+package com.mogikanensoftware.spring.demo.app.projections;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+import com.mogikanensoftware.spring.demo.app.entity.Author;
+
+@Projection(name = "authorInfo", types = Author.class)
+public interface AuthorInfo {
+
+	Long getId();
+
+	@Value("#{target.getFirstName()}")
+	String getFirstName();
+
+	@Value("#{target.getLastName()}")
+	String getLastName();
+
+	@Value("#{target.getFirstName()+' '+target.getLastName()}")
+	String getFullName();
+}
--- src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
+++ src/main/java/com/mogikanensoftware/spring/demo/app/repo/AuthorRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.mogikanensoftware.spring.demo.app.entity.Author;
+import com.mogikanensoftware.spring.demo.app.projections.AuthorInfo;
 
+//@RepositoryRestResource(exported=true, excerptProjection = AuthorInfo.class)
 @RepositoryRestResource(exported=true)
 @PreAuthorize("hasRole('VIEW')")
 public interface AuthorRepository extends PagingAndSortingRepository<Author, Long>{

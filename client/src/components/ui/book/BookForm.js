3d4b5b636afc4636b9cb883881f1e410ec9f8c01
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2017-12-28 22:10:16 -0500

    updated BookForm with publishDate and addAuthor UI components

--- client/src/components/ui/book/BookForm.js
+++ client/src/components/ui/book/BookForm.js
@@ -1,54 +1,77 @@
 import React, { Component } from 'react'
 import { Button } from 'react-bootstrap'
+import DayPickerInput from 'react-day-picker/DayPickerInput'
+import 'react-day-picker/lib/style.css'
 
 export class BookForm extends Component {
 
     constructor(props) {
         super(props)
+        this.handleDayChange = this.handleDayChange.bind(this)
+        this.buildAuthorListOptions = this.buildAuthorListOptions.bind(this)
+    }
+
+    handleDayChange(day) {
+        console.log('selected publicationDate->', day)
+    }
+
+    buildAuthorListOptions(authors){        
+        return authors.map(function(author){
+            return (<option value={author.id}>{author.name}</option>)
+        })
     }
 
     render() {
-        return(
-        <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                    <div className="panel panel-default">
-                        <div className="panel-heading">
-                            <h3 className="panel-title">Add Book</h3>
-                        </div>
-                        <div className="panel-body">
-                            <form>
-                                <div className="form-group">
-                                    <label htmlFor="ISBN">ISBN:</label>
-                                    <input type="text"
-                                        className="form-control"
-                                        id="isbn"></input>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="name">Book Name:</label>
-                                    <input type="text"
-                                        className="form-control"
-                                        id="name"></input>
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="publicationDate">Publication Date:</label>
-                                    <input type="text"
-                                        className="form-control"
-                                        id="publicationDate"></input>
-                                </div>
-                                <div className="form-group">
-                                    <Button
-                                        onClick={this.props.handleHideForm}
-                                        bsStyle="default">Cancel</Button>
-                                    <Button onClick={this.props.handleSubmitForm}
-                                        bsStyle="default"
-                                        type="submit">Submit</Button>
-                                </div>
-                            </form>
+        let authors = [{id:1, name:"John Doe"},{id:2,name:"Jenny Doe"},{id:3, name:"Vasya Pupkin"}]
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-8">
+                        <div className="panel panel-default">
+                            <div className="panel-heading">
+                                <h3 className="panel-title">Add Book</h3>
+                            </div>
+                            <div className="panel-body">
+                                <form>
+                                    <div className="form-group">
+                                        <label htmlFor="addIsbn">ISBN:</label>
+                                        <input type="text"
+                                            className="form-control"
+                                            id="addIsbn"></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="addBookName">Book Name:</label>
+                                        <input type="text"
+                                            className="form-control"
+                                            id="addBookName"></input>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="addPubDate">Publication Date:</label>
+                                        <div>
+                                            <DayPickerInput
+                                                onDayChange={this.handleDayChange} />
+                                        </div>
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="addAuthor">Author:</label>
+                                        <select id="addAuthor" className="form-control">
+                                            {this.buildAuthorListOptions(authors)}
+                                        </select>
+                                    </div>
+                                    <div className="form-group">
+                                        <Button
+                                            onClick={this.props.handleHideForm}
+                                            bsStyle="default">Cancel</Button>
+                                        <Button onClick={this.props.handleSubmitForm}
+                                            bsStyle="default"
+                                            type="submit">Submit</Button>
+                                    </div>
+                                </form>
+                            </div>
                         </div>
                     </div>
+                    <div className="col-md-4"></div>
                 </div>
-            </div>
-        </div>)
+            </div>)
     }
 }

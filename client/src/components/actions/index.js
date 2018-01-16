d0a7398a1469e203add6f4ca468ed3ea41d36ed0
Vladislav Voinov <vladislav.voinov@ontariomd.com>
2018-01-15 20:50:48 -0500

    removed unused action and improve imports readability for books

--- client/src/components/actions/index.js
+++ client/src/components/actions/index.js
@@ -1,5 +1,7 @@
-import { FETCH_BOOKS, DISPLAY_BOOKS, DELETE_BOOK, RESET_NEW_BOOK_PROPS } from '../constants/ActionTypes'
-import { SHOW_ADD_NEW_BOOK, HIDE_ADD_NEW_BOOK, SET_NEW_BOOK_PROPS, UPDATE_VALIDATION_ERRORS } from '../constants/ActionTypes'
+import { FETCH_BOOKS, DISPLAY_BOOKS } from '../constants/ActionTypes'
+import { DELETE_BOOK, RESET_NEW_BOOK_PROPS } from '../constants/ActionTypes'
+import { SHOW_ADD_NEW_BOOK, HIDE_ADD_NEW_BOOK } from '../constants/ActionTypes'
+import { SET_NEW_BOOK_PROPS, UPDATE_VALIDATION_ERRORS } from '../constants/ActionTypes'
 import fetch from 'isomorphic-fetch'
 
 export const fetchBooks = (dispatch, pageSize) => {
